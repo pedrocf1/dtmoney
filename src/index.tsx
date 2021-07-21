b5337222edae40d8cac0e1dc662e5f5eb4df0335
Pedro Freire <pedrofc31.31@gmail.com>
2021-07-21 19:30:32 -0300

    implementing modal and putting new values in the database

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import {App} from './App';
 
 createServer({
+  models:{
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions:[
+        {
+          id: 1,
+          title: 'Freelance bolado',
+          amount: 30000,
+          type: 'deposit',
+          category: 'Dev',
+          createdAt: new Date()
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          amount: 1100,
+          type: 'withdraw',
+          category: 'Casa',
+          createdAt: new Date()
+        }
+      ]
+    })
+  },
+
   routes(){
     this.namespace = 'api';
 
     this.get('/transactions', ()=>{
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request)=>{
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data)
     })
   }
+
+
 })
 
 ReactDOM.render(
